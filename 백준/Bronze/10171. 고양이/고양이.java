c1a6945b2f63decad4532d558910f332aa316c37
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-06-07 18:36:39 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String cat = "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|";
+
+        System.out.println(cat);
+    }
+}
