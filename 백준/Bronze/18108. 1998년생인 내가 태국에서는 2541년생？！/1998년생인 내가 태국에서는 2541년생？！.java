8838d9bdec8cfc06fcbec41929d4f0d91d8c0e8a
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-11-05 15:14:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
