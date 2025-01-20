49772d0883abfdfa57a1c910e25f6a607c718523
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2025-01-20 22:15:07 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonacci(N));
+
+    }
+
+    public static int fibonacci(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
