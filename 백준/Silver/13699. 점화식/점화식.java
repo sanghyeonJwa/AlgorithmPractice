241f43797fb6b1fe4870bfaf747cb3d42c5ee265
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2025-01-22 23:35:12 +0900

    [Silver IV] Title: 점화식, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= N; i++) {
+
+            dp[i] = 0;
+
+            for(int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
