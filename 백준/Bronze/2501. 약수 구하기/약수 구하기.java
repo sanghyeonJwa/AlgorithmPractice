81f54fa71c74d27c6adad2c87354871125aa97e0
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2025-05-04 07:18:39 +0900

    [Bronze III] Title: 약수 구하기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        int result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                cnt++;
+                if (cnt == K) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
